f53c9e81d57c55c9c6b3b049371ac64df299ace6
math314 <314same@gmail.com>
2012-02-18 00:59:03 +0900

    追加できてない

--- ICPC/1136.cpp
+++ ICPC/1136.cpp
@@ -2,46 +2,7 @@
 
 using namespace std;
 
-int money,year;
 
-int Fu(double p,int t){
-	int ret = money;
-	for(int i=0;i<year; i++)
-		ret = (int)(ret * ( 1 + p)) - t;
-	return ret;
-}
-int Ta(double p, int t)
-{
-	int z = money;
-	int r =0;
-	for(int i=0;i<year;i++)
-	{
-		r += (int)(z*p);
-		z-= t;
-	}
-
-	return z+r;
-
-}
-
-int solve()
-{
-	int n;
-	cin >> money >> year >> n;
-	int rets[100];
-	for(int i= 0; i <n; i++){
-		int fukuri;
-		double p;
-		int t;
-		cin >> fukuri >> p >> t;
-		rets[i] = (fukuri)?Fu(p,t):Ta(p,t);
-	}
-	int ret = 0;
-	for(int i=0;i<n;i++)
-		if(ret <rets[i]) ret = rets[i];
-
-	return ret;
-}
 
 int main()
 {
